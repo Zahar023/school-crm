ab488f885a5d797a9eea441eb4fb7f9b2f22b821
Vitalchengo <mr.shchigre@mai.ru>
2025-08-20 13:30:05 +0700

    Добавил time-buttons

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -37,6 +37,15 @@ export default function Dashboard() {
   if (loading) return <div>Загрузка...</div>;
   if (error) return <div>Ошибка: {error}</div>;
 
+  function TimeSlot({time}) {
+
+    function handleClick() {
+      console.log('clicked!');
+    }
+
+    return <button className="square" onClick={handleClick}>{time}</button>;
+  }
+
   return (
     <div style={{ padding: "20px" }}>
       <h1>Личный кабинет</h1>
@@ -46,7 +55,24 @@ export default function Dashboard() {
           <li key={teacher.id}>{teacher.name} </li>
         ))}
       </ul>
-      <button onClick={handleExit}> Выход </button>
+      <div>
+        <TimeButtons>
+          <TimeSlot time="09:00" />
+          <TimeSlot time="10:00" />
+          <TimeSlot time="11:00" />
+          <TimeSlot time="12:00" />
+          <TimeSlot time="13:00" />
+          <TimeSlot time="14:00" />
+          <TimeSlot time="15:00" />
+          <TimeSlot time="16:00" />
+          <TimeSlot time="17:00" />
+          <TimeSlot time="18:00" />
+          <TimeSlot time="19:00" />
+          <TimeSlot time="20:00" />
+          <TimeSlot time="21:00" />
+        </TimeButtons>
+      </div>
+      <button onClick={handleExit}> Выход </button>      
     </div>
   );
 }
