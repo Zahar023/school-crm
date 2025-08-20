14ca61df98e52ca7b88d4db94d7b6a45ade27be7
Vitalchengo <mr.shchigre@mai.ru>
2025-08-20 13:48:44 +0700

    добавил комментарий к функции handleClick

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -40,7 +40,7 @@ export default function Dashboard() {
   function TimeSlot({time}) {
 
     function handleClick() {
-      console.log('clicked!');
+      console.log('clicked!');  //Я брал инфу с документации где по очередно делали крестики-нолики, поэтому эта функция здесь нахер не нужна
     }
 
     return <button className="square" onClick={handleClick}>{time}</button>;
