5b8f188ae9e0f7978e4719e451eaa2291aa3f309
Vitalchengo <mr.shchigre@mai.ru>
2025-08-20 13:45:52 +0700

    restayling

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import axios from "TimeButtons";
 
 export default function Dashboard() {
   const [teachers, setTeachers] = useState([]);
@@ -38,6 +37,15 @@ export default function Dashboard() {
   if (loading) return <div>Загрузка...</div>;
   if (error) return <div>Ошибка: {error}</div>;
 
+  function TimeSlot({time}) {
+
+    function handleClick() {
+      console.log('clicked!');
+    }
+
+    return <button className="square" onClick={handleClick}>{time}</button>;
+  }
+
   return (
     <div style={{ padding: "20px" }}>
       <h1>Личный кабинет</h1>
@@ -47,22 +55,48 @@ export default function Dashboard() {
           <li key={teacher.id}>{teacher.name} </li>
         ))}
       </ul>
-      <div>
-        <TimeButtons>
-          <TimeSlot time="09:00" />
-          <TimeSlot time="10:00" />
-          <TimeSlot time="11:00" />
-          <TimeSlot time="12:00" />
-          <TimeSlot time="13:00" />
-          <TimeSlot time="14:00" />
-          <TimeSlot time="15:00" />
-          <TimeSlot time="16:00" />
-          <TimeSlot time="17:00" />
-          <TimeSlot time="18:00" />
-          <TimeSlot time="19:00" />
-          <TimeSlot time="20:00" />
-          <TimeSlot time="21:00" />
-        </TimeButtons>
+      <div className="time-buttons">
+        <ul>
+          <li>
+            <TimeSlot time="09:00" /> <button>Записать</button>
+          </li>
+          <li>
+            <TimeSlot time="10:00" /> <button>Записать</button>
+          </li>
+          <li>
+            <TimeSlot time="11:00" /> <button>Записать</button>
+          </li>
+          <li>
+            <TimeSlot time="12:00" /> <button>Записать</button>
+          </li>
+          <li>
+            <TimeSlot time="13:00" /> <button>Записать</button>
+          </li>
+          <li>
+            <TimeSlot time="14:00" /> <button>Записать</button>
+          </li>
+          <li>
+            <TimeSlot time="15:00" /> <button>Записать</button>
+          </li>
+          <li>
+            <TimeSlot time="16:00" /> <button>Записать</button>
+          </li>
+          <li>
+            <TimeSlot time="17:00" /> <button>Записать</button>
+          </li>
+          <li>
+            <TimeSlot time="18:00" /> <button>Записать</button>
+          </li>
+          <li>
+            <TimeSlot time="19:00" /> <button>Записать</button>
+          </li>
+          <li>
+            <TimeSlot time="20:00" /> <button>Записать</button>
+          </li>
+          <li>
+            <TimeSlot time="21:00" /> <button>Записать</button>
+          </li>
+        </ul>
       </div>
       <button onClick={handleExit}> Выход </button>      
     </div>
