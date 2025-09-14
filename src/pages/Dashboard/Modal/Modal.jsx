ab66264533a9a0e445b56cd622284f916a0f4d25
Vitalchengo <mr.shchigre@mai.ru>
2025-09-14 22:40:51 +0700

    modal window

--- src/pages/Dashboard/Modal/Modal.jsx
+++ src/pages/Dashboard/Modal/Modal.jsx
@@ -2,8 +2,14 @@ import "./Modal.css";
 import { useEffect } from "react";
 
 export default function Modal({ active, setActive }) {
+    useEffect(() => {
+        const handleEsc = (event) => {
+            if (event.key === "Escape") {
+                setActive(false); // закрыть модальное окно при нажатии Esc
+            }
+        }
+    })
   return (
-
     
     <div className={active ? "modal active" : "modal"}>
       <div className="modal__content" onClick={(e) => e.stopPropagation()}>
